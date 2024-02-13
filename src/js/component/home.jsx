56bcfc28f3d6698eda505c096b36acf7b97a19e9
JParedesArtica <paredesjavier23@gmail.com>
2024-02-13 18:01:40 -0500

    aumento de buttons para creación de luz violeta

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,39 +1,69 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
-
 
 //create your first component
 
 const Home = () => {
-	
-	
-		const [color, setColor] = useState("null");
-		
+  const [activeLight, setActiveLight] = useState("null");
+  const [lightPurple, setLightPurple] = useState(false);
+  const colors = ["red", "yellow", "green"];
 
-		let redExtraClass = " ";
-		if(color === "red") redExtraClass = "selected"
-		let yellowExtraClass = " ";
-		if(color === "yellow") yellowExtraClass = "selected"
-		let greenExtraClass = " ";
-		if(color === "green") greenExtraClass = "selected"
+  const colorIndex = colors.indexOf(activeLight);
 
-		
+  const toggleLight = (lightColor) => {
+    setActiveLight((prevActiveLight) =>
+      prevActiveLight === lightColor ? null : lightColor
+    );
+  };
 
-		return (
-			<div className="container">
-			<div id = "trafficTop"></div>
-			<div id="semaforo">
-			<div className = {"red light " + redExtraClass} onClick={()=>{setColor("red")}}></div>
-			<div className={"yellow light " + yellowExtraClass} onClick={()=>{setColor("yellow")}}></div>
-			<div className={"green light " + greenExtraClass} onClick={()=>{setColor("green")}}></div>
-			</div>
-			
-			</div>
-			
-		  
-		);
-	  };
-	  
+  const changeColor = () => {
+    const nextIndex = (colorIndex + 1) % colors.length;
+    setActiveLight(colors[nextIndex]);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(changeColor, 2000);
+    return () => clearInterval(intervalId);
+  }, [colorIndex]);
+
+  return (
+    <div className="container">
+      <div id="trafficTop"></div>
+      <div id="semaforo">
+        {colors.map((lightColor) => (
+          <div
+            key={lightColor}
+            className={`light ${lightColor} ${
+              activeLight === lightColor ? "selected" : ""
+            }`}
+            onClick={() => {
+              toggleLight(lightColor);
+            }}
+          ></div>
+        ))}
+        {lightPurple && (
+          <div
+            className={`purple light ${
+              activeLight === "purple" ? "selected" : ""
+            } `}
+            onClick={() => {
+              toggleLight("purple");
+            }}
+          ></div>
+        )}
+      </div>
+      <button
+        className="mt-2 btn btn-success"
+        onClick={() => setLightPurple(!lightPurple)}
+      >
+        {lightPurple ? "Remove purple light" : "Add purple light"}
+      </button>
+      <button className="mt-2 btn btn-primary" onClick={changeColor}>
+        Change Color
+      </button>
+    </div>
+  );
+};
 
 export default Home;
